cfed70ca2eba88790dab739084fcac99a5b37743
LanzSalviejo <lanzsalviejo@gmail.com>
2024-10-05 14:44:50 -0700

    styles

--- frontend/src/Body.jsx
+++ frontend/src/Body.jsx
@@ -1,9 +1,13 @@
+import Fridge from "./Fridge";
+import Recipes from "./Recipes";
 
 function Body() {
   return (
     <main className="body">
       <h2>Welcome to my React app!</h2>
       <p>This is a basic React page with a header, nav, body, and footer.</p>
+      <Fridge></Fridge>
+      <Recipes></Recipes>
     </main>
   );
 }
---
+++ frontend/src/Fridge.jsx
@@ -0,0 +1,10 @@
+function Fridge() {
+  return (
+    <main className="fridge">
+      <h2>Fridge</h2>
+      <p>LOTS OF FOOD</p>
+    </main>
+  );
+}
+
+export default Fridge;
